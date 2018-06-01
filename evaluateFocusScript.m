6a9f340ed20e89c867d535154ea27198ede25764
vanessa-king <vanessaking0808@gmail.com>
2018-06-01 12:08:25 -0400

    Update to work with new ROI detection method

--- evaluateFocusScript.m
+++ evaluateFocusScript.m
@@ -1,15 +1,14 @@
-%This is to call the restoreRed and evaluateFocus functions as well as 
-%Focus.ijm and Split.ijm.
+%This is to call the restoreRed and evaluateFocus functions as well as
+%Focus.ijm and Split.ijm
 
 import focusScripts.*
 
-%Here we open the open, and get its the name and path.
+%Here we open the file, and get its the name and path.
 [file,path] = uigetfile('*.tif');
 originalImage = imread(fullfile(path, file));
 %Before we run any functions, we need to first split this image into the
 %green and red sides. This is done in ImageJ because ImageJ retains the
 %photo brightness, which is needed later.
-
 
 %Add path needs to be where the Fiji app is on the computer.
 %Example: 'C:\Users\2ColorTIRF\Downloads\fiji-win64\Fiji.app\scripts'
@@ -28,13 +27,13 @@ ij.IJ.run("Split");
 restoreRed(path);
 
 %Next we want to find ROIs and intensities using the green image and the
-%restored red image.
-impG = ij.IJ.openImage(strcat(path,'restored.png'));
-impG.show();
-%Imports then runs the imagej macro to detect ROIs and intensities.
-ij.IJ.run("Install...", "install="+currentDirectory+"/Focus.ijm");
-ij.IJ.run("Focus");
+%This is done using the findROI function.
+left = imread(strcat(path,'Left.png'));
+right = imread(strcat(path,'restored.png'));
+[leftX,leftY,leftI]=findROI(left,"Left",path,10,2,3,8);
+[rightX,rightY,rightI]=findROI(right,"Right",path,10,2,3,8);
+
 
 %Now we evaluate the tilt of the focuses.
-evaluateFocus(path);
+evaluateFocus(leftX,leftY,leftI,rightX,rightY,rightI);
 %and we're done!

7351688de307acc8d855682e9d36b55748252de5
vanessa-king <vanessaking0808@gmail.com>
2018-06-28 12:05:03 -0400

    Update to current working version

--- findROI.m
+++ findROI.m
@@ -1,4 +1,4 @@
-function [x,y,intensity] = findROI(data, side, filePath, threshold, emitterRadius, exclusionRadius, backgroundRadius)
+function [x,y,intensity] = findROI(data, colour, filePath, threshold, emitterRadius, exclusionRadius, backgroundRadius, clearance)
 %This function is an attempt at using the generateCircles and 
 %intensitytrace functions. It will replace Focus.ijm
 
@@ -28,8 +28,49 @@ intensity = I.' ;
 x = C(:,1);
 y = C(:,2);
 
+%Now we want to remove points outside of our clearance. 
+outlierIndex = [];
+
+%For all ROIs...
+for index = 1:size(x)
+    
+    %If the x value is less than or equal to clearance
+    if x(index) <= clearance
+        %record index
+        outlierIndex = [outlierIndex, index];
+        
+    %If the x value is greater than or equal to width-clearance
+    elseif x(index) >= (imSz(2)-clearance)
+        %record index
+        outlierIndex = [outlierIndex, index];
+  
+    %If the y value is less than or equal to clearance
+    elseif y(index) <= clearance
+        %record index
+        outlierIndex = [outlierIndex, index];
+        
+    %If the y value is greater than or equal to width-clearance
+    elseif y(index) >= (imSz(1)-clearance)
+        %record index
+        outlierIndex = [outlierIndex, index];
+        
+    %If the x value is within the clearance.
+    else %we do nothing
+    end
+end
+
+%We now have a list of all points outside the clearance. Now we can delete
+%them.
+for i = 1:size(outlierIndex)
+    %Delete ROI
+    x(index) = [];
+    y(index) = [];
+    intensity(index) = [];
+end
+
+
 outputTable = table(x,y,intensity);
 %write the data
-writetable(outputTable,filePath+side+'_features.txt');  
+writetable(outputTable,filePath+colour+'_features.txt');  
 
 end
